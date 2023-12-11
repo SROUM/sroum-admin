4e177279273c51d9cde875cc79606291dbab6f4a
Saige <minnsane.kim@gmail.com>
2023-12-11 10:50:00 +0900

    feat: 네비게이션 기타 메뉴 추가

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,6 +1,6 @@
 
 
-<nav class="bg-amber-700 fixed w-full z-20 top-0 start-0 border-b">
+<nav class="bg-amber-700 fixed w-full z-20 top-0 start-0 border-b tracking-wider">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="#none" class="flex items-center space-x-3 rtl:space-x-reverse">
         <span class="self-center text-3xl font-bold text-white whitespace-nowrap">sroum</span>
@@ -8,6 +8,23 @@
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
         <button type="button" class="text-white bg-black/25 hover:bg-blue-800 font-medium rounded-full text-sm px-4 py-2 text-center">LOGOUT</button>
     </div>
+
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+          <li>
+            <a href="#" class="block py-2 px-3 text-white text-lg md:bg-transparent md:p-0 font-medium underline underline-offset-8" aria-current="page">HOME</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 px-3 text-white text-lg md:bg-transparent md:p-0 font-extralight">USER</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 px-3 text-white text-lg md:bg-transparent md:p-0 font-extralight">SPOT</a>
+          </li>
+          <li>
+            <a href="#" class="block py-2 px-3 text-white text-lg md:bg-transparent md:p-0 font-extralight">VISIT</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
   
