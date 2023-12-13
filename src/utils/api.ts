2b455880a8c5002e8c51f2f9c988dffa965d60c6
Saige <minnsane.kim@gmail.com>
2023-12-13 14:47:42 +0900

    feat: axios를 이용한 api 클래스 생성

---
+++ src/utils/api.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export class Api {
+    constructor(){}
+
+    get(url: string, data: any): Promise<any> {
+        return axios.get(url, {data}).then(response => response.data);
+    }
+
+    post(url: string, data: any): Promise<any> {
+        return axios.post(url, {data}).then(response => response.data);
+    }
+
+    put(url: string, data: any): Promise<any> {
+        return axios.put(url, {data}).then(response => response.data);
+    }
+
+    delete(url: string, data: any): Promise<any> {
+        return axios.delete(url, {data}).then(response => response.data);
+    }
+}
