4403e0167247a1f31bec0bcd289578c4f3d33670
Saige <minnsane.kim@gmail.com>
2023-12-14 14:00:08 +0900

    feat: summary mock api 생성 및 홈 컴포넌트 완성

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,1 +1,46 @@
-<h1>Home</h1>
+<script lang="ts">
+  import { Api } from '../utils/api';
+  import { adminUser } from '../utils/stores';
+
+  const api = new Api();
+  $: adminSummary = api.get('/mock/admin_summary.json');
+</script>
+
+<article>
+  <section class="mb-10">
+    <h1 class="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900">
+      ACCESS SUMMARY
+    </h1>
+
+    {#await adminSummary then adminSummary}
+      <ul class="max-w-md space-y-1 text-gray-700 list-disc list-inside">
+        <li>
+          Current Access: {$adminUser?.accessTime}
+        </li>
+        <li>
+          Last Access: {adminSummary.lastAccessTime}
+        </li>
+      </ul>
+    {/await}
+  </section>
+
+  <section>
+    <h1 class="mb-4 text-3xl font-bold leading-none tracking-tight text-gray-900">
+      DATABASE SUMMARY
+    </h1>
+
+    {#await adminSummary then adminSummary}
+      <ul class="max-w-md space-y-1 text-gray-700 list-disc list-inside">
+        <li>
+          Database Status: {adminSummary.database.status}
+        </li>
+        <li>
+          # of Tabled: {adminSummary.database.tableSize}
+        </li>
+        <li>
+          Total # of Entries(Instances): {adminSummary.database.totalEntries}
+        </li>
+      </ul>
+    {/await}
+  </section>
+</article>
